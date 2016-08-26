93c33c79f37db240f36c854b8d1c51b259732689
Jorge Muñoz <punkto@gmail.com>
2016-08-26 18:44:26 +0200

    Initial commit. gdx-setup.jar just executed.

---
+++ desktop/src/ludum/mighty/ld36/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ludum.mighty.ld36.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ludum.mighty.ld36.MightyLD36;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MightyLD36(), config);
+	}
+}
---
+++ html/src/ludum/mighty/ld36/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ludum.mighty.ld36.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ludum.mighty.ld36.MightyLD36;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MightyLD36();
+        }
+}
