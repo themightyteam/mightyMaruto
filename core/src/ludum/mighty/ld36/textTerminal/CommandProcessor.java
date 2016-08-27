9aa709640e025336b70ab74c5e5b0e5f9bb08655
David Chaves <dchavesd@gmail.com>
2016-08-27 14:56:32 +0200

    Command processor work in progress

--- core/src/ludum/mighty/ld36/textTerminal/CommandProcessor.java
+++ core/src/ludum/mighty/ld36/textTerminal/CommandProcessor.java
@@ -2,6 +2,7 @@ package ludum.mighty.ld36.textTerminal;
 
 import ludum.mighty.ld36.actors.Action;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 /**
@@ -10,6 +11,31 @@ import java.util.Vector;
 
 public class CommandProcessor {
     private Vector<Action> commands;
+    private boolean requiresuserinput;
+    private static final String[] validcommands_zero = {"walk", "moonwalk", "run", "punch", "stop", "help"};
+    private static final String[] validcommands_one = {"rotate", "drop", "shoot"};
+    private static final String[] powerups = {"arrrggghhh", "yendor", "choco", "grenade", "random" };
+    private static final String[] directions = {"left","right"};
 
-
+    public void next(String line) {
+        // TODO: parse new received line
+        String[] actions = line.split(";");
+        for (String action:actions) {
+            String[] parts = action.split(" ");
+            if(Arrays.asList(validcommands_zero).contains(parts[0].toLowerCase())) {
+                // Valid command; zero additional parameters
+                String command = parts[0].toLowerCase();
+                if (parts.length != 1) {
+                    // TODO : error "too many parameters"
+                }
+            } else if (Arrays.asList(validcommands_one).contains(parts[0].toLowerCase())) {
+                // Valid command; one additional parameter
+                if (parts.length != 2) {
+                    // TODO : error "invalid parameter number"
+                }
+            } else {
+                // TODO : invalid command
+            }
+        }
+    }
 }
