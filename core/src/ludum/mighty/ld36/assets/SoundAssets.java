4cb6d946605eb7eab08d2cf71ab58f0d47645cb6
Jorge Muñoz Castañer <punkto@gmail.com>
2016-08-28 11:53:52 +0200

    MightyLD36 themes added.

--- core/src/ludum/mighty/ld36/assets/SoundAssets.java
+++ core/src/ludum/mighty/ld36/assets/SoundAssets.java
@@ -7,13 +7,19 @@ import com.badlogic.gdx.audio.Sound;
 public class SoundAssets {
 
 	Music themeSong;
+	Music themeSongSlow;
+	Music themeSongFast;
 
 	Sound hastalavista;
 
 	public SoundAssets() {
 
+		this.themeSongSlow = Gdx.audio.newMusic(Gdx.files
+				.internal("sounds/MightyLD36_slow_1.mp3"));
 		this.themeSong = Gdx.audio.newMusic(Gdx.files
-				.internal("sounds/theme_1.mp3"));
+				.internal("sounds/MightyLD36_normal_1.mp3"));
+		this.themeSongFast = Gdx.audio.newMusic(Gdx.files
+				.internal("sounds/MightyLD36_fast_1.mp3"));
 
 		this.hastalavista = Gdx.audio.newSound(Gdx.files
 				.internal("sounds/hasta_la_vista.wav"));
@@ -27,6 +33,26 @@ public class SoundAssets {
 	public void stopTheme() {
 		this.themeSong.stop();
 		this.themeSong.setLooping(true);
+	}
+
+	public void playThemeSlow() {
+		this.themeSongSlow.play();
+		this.themeSongSlow.setLooping(true);
+	}
+
+	public void stopThemeSlow() {
+		this.themeSongSlow.stop();
+		this.themeSongSlow.setLooping(true);
+	}
+
+	public void playThemeFast() {
+		this.themeSongFast.play();
+		this.themeSongFast.setLooping(true);
+	}
+
+	public void stopThemeFast() {
+		this.themeSongFast.stop();
+		this.themeSongFast.setLooping(true);
 	}
 
 	public void playHastalavista() {
