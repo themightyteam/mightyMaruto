f7b8057cc6f7bb6e5fe1c9f185e32b6df2a9115e
Jorge Muñoz Castañer <punkto@gmail.com>
2016-08-29 19:28:16 +0200

    set turn time to 3 secs

--- core/src/ludum/mighty/ld36/settings/DefaultValues.java
+++ core/src/ludum/mighty/ld36/settings/DefaultValues.java
@@ -81,7 +81,7 @@ public class DefaultValues {
     // World constants
     public static final int TILESIZE = 32;
 
-	public static final int WORLD_SECONDS_FOR_COMMAND_INPUT = 2;
+	public static final int WORLD_SECONDS_FOR_COMMAND_INPUT = 3;
 
 	// World state
 	/*
